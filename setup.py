e88c302ab123a2f9703e3ba1a040b22885df6ef5
BitByte <adilrahman20008@gmail.com>
2023-02-01 16:38:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='fastBart',
     version='1.0',
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=[
     'torch>=1.7.0,!=1.8.0', # excludes torch v1.8.0
     'onnx',
