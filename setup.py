51dfd8959bd4fe90b6f807e0399e55e52dd12e54
BitByte <adilrahman20008@gmail.com>
2023-02-01 16:37:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='My First Setup File',
+    name='fastBart',
     version='1.0',
     packages=find_packages()
+    install_requires=[
+    'torch>=1.7.0,!=1.8.0', # excludes torch v1.8.0
+    'onnx',
+    'onnxruntime==1.7.0',
+    'transformers==4.11.3',
+    'progress>=1.5',
+    'sentencepiece',
+    'psutil',
+    'numpy',
+    'matplotlib',
+    ]
 )
